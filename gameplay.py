78e3665ea070747b6aec0cad0afa13b1577e6efd
Pacemaker <106478438+pacemxker@users.noreply.github.com>
2023-01-12 09:43:05 +0100

    Add files via upload

---
+++ gameplay.py
@@ -0,0 +1,34 @@
+from pystyle import *
+from textes import debut
+import param
+import threading
+
+def proc():
+  debut()
+  import Laby
+  Laby.Labyrinthe()
+
+def proc2():
+  import temps
+  temps.calctemps()
+
+def accueil():
+  Write.Print("Bienvenue dans The Maze Walker \n", Colors.white, interval=0.05)
+  action = Write.Input("Que souhaitez-vous faire ? \n\
+Parametres - Jouer - Quitter \n", Colors.white, interval=0.05)
+
+  if action.lower() == "parametres":
+    param.parametres()
+
+  if action.lower() == "jouer":
+    th1.start()
+    th2.start()
+
+  if action.lower() == "quitter":
+    quit()
+
+th1 = threading.Thread(target=proc)
+th2 = threading.Thread(target=proc2)
+
+if __name__ == "__main__":
+  accueil()
